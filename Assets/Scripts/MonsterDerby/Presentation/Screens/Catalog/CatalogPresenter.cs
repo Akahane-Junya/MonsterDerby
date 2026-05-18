d84bfcb063d04077a8b09bee6478aee8854ee029
junya.akahane <survive294@gmail.com>
2026-05-18 23:04:30 +0900

    fix: CatalogPresenterでLabel型のusing漏れ修正

--- Assets/Scripts/MonsterDerby/Presentation/Screens/Catalog/CatalogPresenter.cs
+++ Assets/Scripts/MonsterDerby/Presentation/Screens/Catalog/CatalogPresenter.cs
@@ -1,6 +1,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UIElements;
 using MonsterDerby.Presentation.Navigation;
 
 namespace MonsterDerby.Presentation.Screens.Catalog
